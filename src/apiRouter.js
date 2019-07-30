a646edf43fac142527bdcafaeed1d0152778734c
John Afolayan <jnafolayan@gmail.com>
2019-07-30 14:29:04 +0100

    initial commit

---
+++ src/apiRouter.js
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+
+export function setup() {
+  const apiRouter = Router();
+
+  return apiRouter;
+}
---
+++ src/util.js
@@ -0,0 +1,5 @@
+export function createError(status, message) {
+  const error = new Error(message);
+  error.status = status;
+  return error;
+}
